ae69a52780a237441a708fbef941d159479f8aa0
Diego <diannewdev@gmail.com>
2024-09-23 00:56:47 -0400

    completei o array das imagens das cartas

--- src/data/ArmazemDeCartas.ts
+++ src/data/ArmazemDeCartas.ts
@@ -3,10 +3,22 @@ const cartasOriginal: string[] = [
     './assets/images/cartas/vizir.jpg',
     './assets/images/cartas/sacerdotisa.jpg',
     './assets/images/cartas/general.jpg',
-    './assets/images/cartas/crianca.jpg',
+    './assets/images/cartas/artesao.jpg',
     './assets/images/cartas/escriba.jpg',
     './assets/images/cartas/filha.jpg',
+    './assets/images/cartas/guarda.jpg',
+    './assets/images/cartas/crianca.jpg',
+    './assets/images/cartas/escravos.jpg',
+    './assets/images/cartas/nobreza.jpg',
+    './assets/images/cartas/guardiao.jpg',
+    './assets/images/cartas/rainha.jpg',
     './assets/images/cartas/farao.jpg',
+    './assets/images/cartas/engenheiro.jpg',
+    './assets/images/cartas/embalsamador.jpg',
+    './assets/images/cartas/ritualista.jpg',
+    './assets/images/cartas/sacerdote.jpg',
+    './assets/images/cartas/loucos.jpg',
+    './assets/images/cartas/astronomo.jpg',
     ]
 
     export default cartasOriginal;
